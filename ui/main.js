6d447394173c1b683cb73378e1a90e2e9236ed98
Soumodeep <soumodeepp@gmail.com>
2017-08-13 16:19:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,6 +20,6 @@ button.onclick = function () {
         //Not done yet
     };
    //Make the request
-   request.open('GET','http://http://soumodeepp96.imad.hasura-app.io/counter',true);
+   request.open('GET','http://http://soumodeepp96.imad.hasura-app.io/counter', true);
    request.send(null);
 };
