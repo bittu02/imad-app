733a49b5310d25adf96409a9e68a95d3e5cc0959
Soumodeep <soumodeepp@gmail.com>
2017-08-13 13:03:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
 
 //Move the image
-var img = document.getelementById('img');
+var img = document.getElementById('img');
 img.onclick = function() {
     img.style.marginLeft = '100px';
 };
