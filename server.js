6b972b6c22ecab399ef3e0bfea7c25c6e477989c
Soumodeep <soumodeepp@gmail.com>
2017-08-16 21:27:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ app.get('/', function (req, res) {
        if(err) {
            res.status(500),send(err.toString());
        } else {
-           res.send(JSON.stringify(result));
+           res.send(JSON.stringify(result.rows));
        }
     });    
 
