ec7bc54c53a699c98e0619269dfafc120cf2ca81
Soumodeep <soumodeepp@gmail.com>
2017-08-16 21:26:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,9 +96,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/test-db',function(req, res) {
+    var pool = new Pool(config);
+    app.get('/test-db',function(req, res) {
     //make a select request and return response with results
-    
+    pool.query('SELECT * FROM test',function(err, result) {
+       if(err) {
+           res.status(500),send(err.toString());
+       } else {
+           res.send(JSON.stringify(result));
+       }
+    });    
 
 });
 
