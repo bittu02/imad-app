bbe91c1d6d5e77948ce247ca50dcf73be379e4a8
Soumodeep <soumodeepp@gmail.com>
2017-08-16 22:08:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,46 +15,7 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 
-var articles = {
-    'article-one' : {
-        title : 'Article One | Soumodeep',
-        heading: 'Article One',
-        date : 'August 13, 2017',
-        content :   `<p>
-                        This is the content for my first article.  This is the content for my first article.
-                         This is the content for my first article.  This is the content for my first article.
-                          This is the content for my first article.  This is the content for my first article.
-                    </p>
-                    <p>
-                        This is the content for my first article.  This is the content for my first article.
-                         This is the content for my first article.  This is the content for my first article.
-                          This is the content for my first article.  This is the content for my first article.
-                    </p>
-                    <p>
-                        This is the content for my first article.  This is the content for my first article.
-                         This is the content for my first article.  This is the content for my first article.
-                          This is the content for my first article.  This is the content for my first article.
-                    </p>`
-                },
-    'article-two' : {
-        title : 'Article Two | Soumodeep',
-        heading: 'Article Two',
-        date : 'August 14, 2017',
-        content :   `<p>
-                        This is the content for my second article.
-                        
-                    </p>`
-                },
-    'article-three' : {
-        title : 'Article Three | Soumodeep',
-        heading: 'Article Three',
-        date : 'August 15, 2017',
-        content :   `<p>
-                        This is the content for my third article.
-                        
-                    </p>`
-    }
-};
+
 function createTemplate (data) {
     var title = data.title;
     var date = data.date;
