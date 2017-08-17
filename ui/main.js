f34dc47ca3dc1d950229db5c3acdd97a0cf9dea5
Soumodeep <soumodeepp@gmail.com>
2017-08-17 22:04:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,44 +1,29 @@
-//Counter code
-var button = document.getElementById('counter');
-
-button.onclick = function () {
-    
-    //Create a request object
-    var request = new XMLHttpRequest();
-    
-    //Capture the response and store it in a variable
-    request.onreadystatechange = function() {
-        if (request.readyState === XMLHttpRequest.DONE) {
-            //Take some action
-            if(request.status === 200) {
-                    var counter = request.responseText;
-                    var span = document.getElementById('count');
-                    span.innerHTML = counter.toString();
-            }
-            
-        }
-        //Not done yet
-    };
-    
-   //Make the request
-   request.open('GET','http://soumodeepp96.imad.hasura-app.io/counter', true);
-   request.send(null);
-};
-
-
-//Submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+//Submit username/apssword to login
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
-    //Make a request to the server and send the name
     
-    //Capture a list of names and render it as a list
-        var names = ['name1','name2','name3','name4'];
-        var list = '';
-        for(var i=0; i<names.length; i++) {
-            list == '<li>' + names[i] +'</li>';
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function() {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+              console.log('user logged in');
+              alert('Logged in successfully');
+            } else if (request.status === 403) {
+                alert('username/password is incorrect!');
+            } else if (request.status === 500) {
+                alert('something went wrong on the server');
+            }
         }
-        var ul = document.getElementById('namelist');
-        ul.innerHTML = list;
+    };
+    
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST','http://soumodeepp96.imad.hasura-app.io/login', true);
+        request.setRequestHeader('Content-type','application/json');
+        request.send(JSON.stringify({username: username, password: password}));
+        
+        
 };
